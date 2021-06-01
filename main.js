e5d62668ad9ad44108566b41247912e6903511af
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-06-01 14:07:30 +0200

    działa z domyślną wartością, niestety nie dodaje kolejnych obostrzeń do kliknięciu

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const input = document.querySelector("#fnumber");
-const btn = document.querySelector("button");
-const list = document.querySelector(".list");
-
+const input = document.getElementById("fnumber");
+const btn = document.getElementById("btn");
+const list = document.getElementById("list");
 let inputValue = input.value;
-console.log(input);
 
 
 const las = "lasy zostają zamknięte na dwa tygodnie";
@@ -11,10 +9,10 @@ const galerie = "galerie zostają zamknięte przynajmniej do końca miesiąca";
 const maseczki = "wymóg noszenia maseczek na dworze";
 const fryzjerzy = "zakłady fryzjerskie nadal pozostają zamknięte";
 const sport = "obiekty sportowe za wyjątkiem tych na świeżych powietrzu pozostają zamknięte";
-const siłka = "o siłowniach nikt już nie pamięta";
+const silka = "o siłowniach nikt już nie pamięta";
 const restauracje = "w restauracjach tylko ogródki";
 const kina = "w kinach tylko filmy polskie";
-const szkoły = "nauka hybrydowa przynajmniej do 2023";
+const szkoly = "nauka hybrydowa przynajmniej do 2023";
 const zgromadzenia = "zakaz zgromadzeń powyżej 1 osoby";
 const church = "kościoły strefą wolną od covid";
 const wybory = "wybory zależne od tego kto prowadzi w sondażach";
@@ -26,32 +24,33 @@ const arrayList = [
   maseczki,
   fryzjerzy,
   sport,
-  siłka,
+  silka,
   restauracje,
   kina,
-  szkoły,
+  szkoly,
   zgromadzenia,
   church,
-  wybory,
+  wybory
 ];
 
 
-function getRandom() {
+function getRandom(inputValue) {
   if (inputValue > 5000) {
     let random = Math.floor(Math.random() * arrayList.length);
     return arrayList[random];
   } else {
-    alert("za mało zakażeń");
+    return "za mało zakażeń";
   }
 }
 //niech pod random będzie liczba, która będzie indeksem tablicy i wtedy return zwróci tablicę o konkretnym indeksie
 
-function addNewRest(event) {
+function addNewRest(inputValue) {
   const taskDiv = document.createElement("div");
-  taskDiv.textContent = getRandom();
+  taskDiv.textContent = getRandom(inputValue);
   taskDiv.classList.add("rest");
   list.appendChild(taskDiv);
   // list to nazwa klasy w której są obostrzenia. klasa to div class='list'
 }
 
-btn.addEventListener("click", addNewRest);
+// btn.addEventListener("click", addNewRest);
+btn.addEventListener('click', addNewRest(inputValue));
